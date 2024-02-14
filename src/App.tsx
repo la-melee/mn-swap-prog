a019b964b89397c105fd011dc9780cf58001f8a1
Thomas <thomas@Thomass-MacBook-Pro.local>
2024-02-14 12:12:09 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { initClient, ClientProvider } from "@swapcard/react-sdk/lib/client";
+import { ApplicationProvider } from "@swapcard/react-sdk/lib/application";
+import { EventProvider } from "@swapcard/react-sdk/lib/event";
 
 function App() {
+  const client = initClient();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ClientProvider client={client}>
+    <ApplicationProvider>
+      <EventProvider eventId="RXZlbnRfMTIxNzkxMg==">
+        <h1>ok</h1>
+      </EventProvider>
+    </ApplicationProvider>
+  </ClientProvider>
   );
 }
 
